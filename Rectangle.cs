544fd2b48f028635cd9b7c279663bea81fa54c00
annem <33720687+annemik@users.noreply.github.com>
2018-10-20 15:07:04 +0200

    Add files via upload

---
+++ Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+class Rectangle : Shape
+{
+	private int width, height;
+	
+    public Rectangle(int x, int y, int width, int height)
+    {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+    }
+    
+	public override void Draw(Graphics Canvas)
+    {
+		Pen pen = new Pen(Color.Black);
+		Canvas.DrawLine(pen,x,y,x + width,y);
+		Canvas.DrawLine(pen,x+width,y,x+width,y+height);
+		Canvas.DrawLine(pen,x+width,y+height,x,y+height);
+		Canvas.DrawLine(pen,x,y+height,x,y);
+    }
+}
+
---
+++ Shape.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Drawing;
+
+public abstract class Shape
+{
+    protected int x, y;
+
+    protected Shape()
+	{
+	}
+
+    public abstract void Draw(Graphics Canvas);
+	
+}
